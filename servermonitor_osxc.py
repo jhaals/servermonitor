8faf2c6be7a0d7d5b7a43bf653d280db8a056e86
Johan Haals <johan.haals@gmail.com>
2009-04-09 14:17:51 +0200

    Removed old version comment

--- servermonitor_osxc.py
+++ servermonitor_osxc.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-#    Version 0.1
 #    Copyleft 2008-2009 Servermonitor
 #    Servermonitor is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
