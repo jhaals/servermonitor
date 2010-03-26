21b203b3b0525d35eb46ef94fa2e32c97d9e386a
Daniel Beecham <joshu@lunix.se>
2010-03-27 00:04:00 +0100

    Ports now load from configfile.

--- linux/servermonitor_linux.py
+++ linux/servermonitor_linux.py
@@ -6,26 +6,11 @@ from __future__ import with_statement
 import urllib
 import commands, re, socket, ConfigParser, os, sys
 
-# Change this to values
-#id = # Example: id = 244 (id given by servermonitor)
-#password = "" # Example: password = "GHJdf76(/&sfsdgkjh" (password given by servermonitor)
+# configfile path.
 configpath = os.path.expanduser("~") + "/.servermonitor.rc"
 
-# Standard ports for services (can be modified if you use a custom port)
-SSH_PORT = 22
-FTP_PORT = 21
-HTTP_PORT = 80
-HTTPS_PORT = 443
-SMTP_PORT = 25
-IMAP_PORT = 143
-POP_PORT = 993
-AFP_PORT = 548
-SMB_PORT = 445
-MySQL_PORT = 3306
-DNS_PORT = 53
-LDAP_PORT = 389
-
-# DO NOT change here unless you know what you're doing
+# Default service ports.
+services = {"SSH":443, "SMTP":25, "IMAP":143, "POP":993, "AFP":548, "SMB":554, "MySQL":3306, "DNS":53, "LDAP":389}
 
 VERSION = 'linux_1.4' # VERION OF SERVERMONITOR
 """
@@ -53,6 +38,18 @@ def get_system_load(): # {{{
         load = [value.replace(',', '.') for value in load]
 
     return load
+# }}}
+
+def CheckService(port): # {{{
+    "CheckService connects to a service to see if it responds."
+    serviceSocket = socket.socket()
+    serviceSocket.settimeout(0.25)
+    try:
+        serviceSocket.connect(('localhost', port))
+        serviceSocket.close()
+        return True
+    except socket.error:
+        return False
 # }}}
 
 if __name__ == '__main__':
@@ -115,31 +112,18 @@ load = re.findall('(\d+[,.]\d+)', uptime)[2] # getting the 15min load for buildi
 p = urllib.urlencode({'password': password, 'hostname': hostname, 'id': id, 'who': who, 'last': last, 'dighost': dighost, 'digreverse': digreverse, 'df': df, 'uname': uname, 'uptime': uptime, 'ifconfig': ifconfig, 'version': VERSION, 'load': load})
 f = urllib.urlopen('http://servermonitor.linuxuser.se/monitor.php', p)
 
-# This CheckService function connects to the service to see if it responds
-def CheckService(port):
-    serviceSocket = socket.socket()
-    serviceSocket.settimeout(0.25)
+for service, value in services.items():
     try:
-        serviceSocket.connect(('localhost', port))
-        serviceSocket.close()
-        return True
-    except socket.error:
-        return False
+        port = config.get("ports", str(service))
+    except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
+        port = value
 
-HTTP = 1 if CheckService(HTTP_PORT) else 0
-HTTPS = 1 if CheckService(HTTP_PORT) else 0
-DNS = 1 if CheckService(DNS_PORT) else 0
-SMB = 1 if CheckService(SMB_PORT) else 0
-AFP = 1 if CheckService(AFP_PORT) else 0
-SSH = 1 if CheckService(SSH_PORT) else 0
-IMAP = 1 if CheckService(IMAP_PORT) else 0
-SMTP = 1 if CheckService(SMTP_PORT) else 0
-FTP = 1 if CheckService(FTP_PORT) else 0
-LDAP = 1 if CheckService(LDAP_PORT) else 0
-# EOF CheckService BLOCK
+    services[service] = 1 if CheckService(int(port)) else 0
+
+services.update({"id":id, "password":password, "LOAD_warning":LOAD_warning});
 
 # Sending info over running services.
-services = urllib.urlencode({ 'id': id, 'password': password, 'HTTP': HTTP, 'SMB': SMB, 'AFP': AFP, 'SSH': SSH, 'DNS': DNS, 'IMAP': IMAP, 'FTP': FTP, 'SMTP': SMTP, 'HTTPS': HTTPS, 'LDAP': LDAP, 'LOAD_warning': LOAD_warning})
-f = urllib.urlopen('http://servermonitor.linuxuser.se/services.php', services)
+servicesHandle = urllib.urlencode(services);
+f = urllib.urlopen('http://servermonitor.linuxuser.se/services.php', servicesHandle)
 
 # vim: set expandtab shiftwidth=4 tabstop=4
