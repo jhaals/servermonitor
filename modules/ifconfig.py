2d09f56093d863af84cba4254eb2dd930ded821e
Daniel Beecham <joshu@lunix.se>
2010-03-29 18:29:56 +0200

    Added ifconfig module.

---
+++ modules/ifconfig.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# Copyleft 2010, Daniel Beecham <joshu@lunix.se>
+# All rights reversed.
+
+"""This module will get and send ifconfig info."""
+
+import cnf;
+import commands;
+import urllib;
+
+def main():
+    """servermonitor.ifconfig main function.
+    Get interface info and send it to servermonitor servers."""
+
+    # Get `ifconfig`
+    output = commands.getoutput("ifconfig -a")
+
+    # Make it a dict along with id and password.
+    data = {"data":output, "id":cnf.id, "password":cnf.password}
+
+    # And send it.
+    handle = urllib.urlopen(cnf.addr + "/handlers/ifconfig.php", urllib.urlencode(data))
+    print "ifconfig: " + handle.read()
+
+# vim: expandtab tabstop=4 shiftwidth=4
