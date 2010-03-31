539c56c44b31c5408a85c04ba9340664f17b701c
Johan Haals <louie@johan-2.local>
2010-03-31 14:17:03 +0200

    Checking version against or website.

--- servermonitor.py
+++ servermonitor.py
@@ -6,17 +6,23 @@ import sys
 import os
 import getopt
 import cnf
+import urllib
 
 # Modules needs to be in python path.
 sys.path.append("./modules")
 
 if __name__ == "__main__":
-
+    
     VERSION = '2.0.0'
     # Make configparser do it's thing.
     cnf.main()
+
+    # Checking for latest version.
     if not cnf.quiet:
-        print 'Now running version %s of Servermonitor' % VERSION
+        latest_version = urllib.urlopen(cnf.addr + "/handlers/latest.php", urllib.urlencode(''))
+        print 'Now running version %s of Servermonitor.' % VERSION
+        if VERSION != latest_version.read():
+            print 'A later version of this software is available for download.' 
 
     # And now, finally, include and run all modules.
     for file in os.listdir("./modules"):
@@ -25,4 +31,5 @@ if __name__ == "__main__":
             exec(file[0:-3] + ".main()")
     if not cnf.quiet:
         print 'Done!'
+        print cnf.addr
 # vim: expandtab tabstop=4 shiftwidth=4
