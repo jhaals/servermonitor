a3cf970fbb95a532a34ab8e14e0248bb36bece4d
Johan Haals <louie@johan-2.local>
2010-04-01 11:17:21 +0200

    Only runs with Mac systems

--- modules/mac.py
+++ modules/mac.py
@@ -9,28 +9,31 @@ import commands
 import urllib
 import sys
 import re
+
 def main():
     """servermonitor.mac main function."""
 
-    if not cnf.quiet:
-        print "Serial+Build: ",
+    if sys.platform == 'darwin':
 
-    # Get serial number
-    serial = commands.getoutput('/usr/sbin/ioreg -l | /usr/bin/grep IOPlatformSerialNumber')	 	
-    serial = re.search(r'IOPlatformSerialNumber" = "([^"]+)', serial).group(1)
+        if not cnf.quiet:
+            print "Serial+Build: ",
+
+        # Get serial number
+        serial = commands.getoutput('/usr/sbin/ioreg -l | /usr/bin/grep IOPlatformSerialNumber')	 	
+        serial = re.search(r'IOPlatformSerialNumber" = "([^"]+)', serial).group(1)
     
-    # Get Mac OS X version and system build
-    sw_vers = commands.getoutput('sw_vers')
-    osx_version = sw_vers.split(':')[2].split('\n')[0].replace('\t','')
-    build_version = sw_vers.split(':')[3].replace('\t','')
+        # Get Mac OS X version and system build
+        sw_vers = commands.getoutput('sw_vers')
+        osx_version = sw_vers.split(':')[2].split('\n')[0].replace('\t','')
+        build_version = sw_vers.split(':')[3].replace('\t','')
 
-    # Make it a dict along with id and password.
-    data = {'serial':serial, 'build_version':build_version, 'osx_version':osx_version,'id':cnf.id, 'password':cnf.password}
+        # Make it a dict along with id and password.
+        data = {'serial':serial, 'build_version':build_version, 'osx_version':osx_version,'id':cnf.id, 'password':cnf.password}
 
-    # And send it.
-    handle = urllib.urlopen(cnf.addr + "/handlers/mac.php", urllib.urlencode(data))
+        # And send it.
+        handle = urllib.urlopen(cnf.addr + "/handlers/mac.php", urllib.urlencode(data))
 
-    if not cnf.quiet:
-        print handle.read()
+        if not cnf.quiet:
+            print handle.read()
 
 # vim: expandtab tabstop=4 shiftwidth=4
